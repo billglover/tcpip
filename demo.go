107ff710b01c316781948e2eb5b8ce6de4db66e3
Bill Glover <bill@billglover.co.uk>
2017-03-07 21:41:02 +0000

    Add STRING handling capability

--- demo.go
+++ demo.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"io"
 	"log"
 	"net"
+	"strconv"
+	"strings"
 
 	"github.com/pkg/errors"
 )
@@ -60,10 +63,79 @@ func server() (e error) {
 }
 
 func client(ip string) (e error) {
+
+	addr := ip + port
+	log.Println("client:", addr)
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		return errors.Wrap(err, "client: dialing "+addr+" failed")
+	}
+	rw := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
+
+	log.Println("client: send the string request")
+	n, err := rw.WriteString("STRING\n")
+	if err != nil {
+		return errors.Wrap(err, "client: could not send the STRING request ("+strconv.Itoa(n)+" bytes written)")
+	}
+	n, err = rw.WriteString("Additional data.\n")
+	if err != nil {
+		return errors.Wrap(err, "client: could not send additional STRING data ("+strconv.Itoa(n)+" bytes written)")
+	}
+	log.Println("client: flush the buffer")
+	err = rw.Flush()
+	if err != nil {
+		return errors.Wrap(err, "client: flush failed")
+	}
+	log.Println("client: read the reply")
+	response, err := rw.ReadString('\n')
+	if err != nil {
+		return errors.Wrap(err, "client: Failed to read the reply: '"+response+"'")
+	}
+
+	log.Println("client: STRING request - got a response:", response)
+
 	return
 }
 
 func handleMessages(c net.Conn) {
-	io.Copy(c, c)
+	rw := bufio.NewReadWriter(bufio.NewReader(c), bufio.NewWriter(c))
 	defer c.Close()
+
+	for {
+		log.Println("server: waiting for command")
+		cmd, err := rw.ReadString('\n')
+		switch {
+		case err == io.EOF:
+			log.Println("server: reached EOF - close this connection")
+			return
+		case err != nil:
+			log.Println("server: error reading command - got: '"+cmd+"'", err)
+			return
+		}
+
+		cmd = strings.Trim(cmd, "\n")
+		log.Println("server: received command '" + cmd + "'")
+		handleStrings(rw)
+	}
+}
+
+func handleStrings(rw *bufio.ReadWriter) {
+	s, err := rw.ReadString('\n')
+	if err != nil {
+		log.Println("server: cannot read from connection", err)
+	}
+
+	s = strings.Trim(s, "\n")
+	log.Println("server:", s)
+
+	_, err = rw.WriteString("Thank you.\n")
+	if err != nil {
+		log.Println("server: cannot write to connection", err)
+	}
+
+	err = rw.Flush()
+	if err != nil {
+		log.Println("server: cannot flush connection", err)
+	}
+
 }
